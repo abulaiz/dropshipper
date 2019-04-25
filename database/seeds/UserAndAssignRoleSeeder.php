93b3e20fb2f0309b6d3b5fc66e1143946905808a
fandidali <crackmr88@gmail.com>
2019-04-25 12:02:39 +0700

    UserAssignRoleSeeder

--- database/seeds/UserAndAssignRoleSeeder.php
+++ database/seeds/UserAndAssignRoleSeeder.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Seeder;
 
 use Spatie\Permission\Models\Role;
 use App\User;
-use Illuminate\Support\Str;
 
 class UserAndAssignRoleSeeder extends Seeder
 {
@@ -15,51 +14,33 @@ class UserAndAssignRoleSeeder extends Seeder
      */
     public function run()
     {
-       $roles = ['admin', 'superadmin', 'member'];
-       $role = [
-       		'admin' => null,
-        	'superadmin' => null,
-        	'member' => null
-       ];
+        if (!User::where('email', 'admin@gmail.com')->exists()) {
+            $user1 = new User();
+            $user1->name = 'admin';
+            $user1->email = 'admin@gmail.com';
+            $user1->password = bcrypt('admin1');
+            $user1->save();
+            $user1->assignRole('admin');
+        }
 
-       $users = [
-       		// Member -> User
+        if (!User::where('email', 'mimin@gmail.com')->exists()) {
+            $user2 = new User();
+            $user2->name = 'mimin';
+            $user2->email = 'mimin@gmail.com';
+            $user2->password = bcrypt('mimin1');
+            $user2->save();
+            $user2->assignRole('superadmin');
+        }
 
-        	['name' => 'create_order_product', 'roles' => ['member']],
-        	['name' => 'user_product_stock', 'roles' => ['member']],
-
-
-        	// Admin & Superadmin -> User
-
-        	['name' => 'add_stock_product', 'roles' => ['admin', 'superadmin']],
-        	['name' => 'product_stock_list', 'roles' => ['admin', 'superadmin']],
-        	['name' => 'product_stock_history', 'roles' => ['admin', 'superadmin']],
-
-        	// Superadmin -> User
-
-        	['name' => 'view_order_last_update_status', 'roles' => ['superadmin']],
-        	['name' => 'reject_sending_request', 'roles' => ['superadmin']],
-       ];
-
-       //Seed Role Data
-       foreach ($roles as $value) {
-       		if( !Role::where('name',$value)->exists() )
-        		$role[$value] = Role::create(['name' => $value]);
-        	else
-        		$role[$value] = Role::findByName($value);
-        	}
-
-		//Seed User Data
-        foreach ($users as $item) {
-        	if( !User::where('name', $item['name'])->exists() ){
-        		$p =  User::create(['name' => $item['name'],
-        							'email' => Str::random(10).'@gmail.com', 
-        							'password' => bcrypt('secret')
-        						  ]);
-        		foreach ($item['roles'] as $value) {
-        			$p->assignRole( $role[$value] );	
-        		}
-        	}
+        if (!User::where('email', 'admin1@gmail.com')->exists()) {
+            $user3 = new User();
+            $user3->name = 'admin1';
+            $user3->email = 'admin1@gmail.com';
+            $user3->password = bcrypt('admin2');
+            $user3->save();
+            $user3->assignRole('member');
         }
     }
 }
+
+
