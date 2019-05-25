7d9e5d47d2cc637c9582161e8fceb451a235c29a
fandidali <crackmr88@gmail.com>
2019-05-25 23:23:45 +0700

    update

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -12,24 +12,438 @@ class ProductSeeder extends Seeder
     public function run()
     {
     	$prod = new Product();
-    	$prod->name = 'Super Boy';
-    	$prod->qty = 100;
-    	$prod->price = 20000;
-    	$prod->type = 'Philek';
-    	$prod->save();
+        $prod->name = 'Super Grow Up';
+        $prod->qty = 1;
+        $prod->price = 485000;
+        $prod->type = 'Paket';
+        $prod->save();
 
-    	$prod1 = new Product();
-    	$prod1->name = 'Super Nil';
-    	$prod1->qty = 200;
-    	$prod1->price = 30000;
-    	$prod1->type = 'Phil';
-    	$prod1->save();
+        $prod1 = new Product();
+        $prod1->name = 'Smart Weight';
+        $prod1->qty = 1;
+        $prod1->price = 403000;
+        $prod1->type = 'Paket';
+        $prod1->save();
 
-    	$prod2 = new Product();
-    	$prod2->name = 'Super Super';
-    	$prod2->qty = 300;
-    	$prod2->price = 20000;
-    	$prod2->type = 'Phi';
-    	$prod2->save();
+        $prod2 = new Product();
+        $prod2->name = 'Perfect Eye';
+        $prod2->qty = 1;
+        $prod2->price = 561000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'PB';
+        $prod2->qty = 1;
+        $prod2->price = 605000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'PBM';
+        $prod2->qty = 1;
+        $prod2->price = 373000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Perfect Slim';
+        $prod2->qty = 1;
+        $prod2->price = 410000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Super Grow Up Mini + Bonus';
+        $prod2->qty = 1;
+        $prod2->price = 265000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Smart Weight Mini';
+        $prod2->qty = 1;
+        $prod2->price = 160000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'PB Mini';
+        $prod2->qty = 1;
+        $prod2->price = 295000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Perfect Slim Mini';
+        $prod2->qty = 1;
+        $prod2->price = 285000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Super Grow Up';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Smart Weight';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Perfect Eye';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse PB';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse PBM';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Perfect Mask';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Box';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Calcium';
+        $prod2->qty = 1;
+        $prod2->price = 230000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Calcium Children';
+        $prod2->qty = 1;
+        $prod2->price = 243000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Calcium Shutang';
+        $prod2->qty = 1;
+        $prod2->price = 282000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Chewable';
+        $prod2->qty = 1;
+        $prod2->price = 243000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Jiang Zhi Tea';
+        $prod2->qty = 1;
+        $prod2->price = 130000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Renuves';
+        $prod2->qty = 150;
+        $prod2->price = 511000;
+        $prod2->type = 'Kaps';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Vitaline';
+        $prod2->qty = 1;
+        $prod2->price = 310000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Spirulina';
+        $prod2->qty = 100;
+        $prod2->price = 343000;
+        $prod2->type = 'Kaps';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Chitosan';
+        $prod2->qty = 1;
+        $prod2->price = 429000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Double Cellulose';
+        $prod2->qty = 1;
+        $prod2->price = 255000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Muncord';
+        $prod2->qty = 1;
+        $prod2->price = 470000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Grape Extract';
+        $prod2->qty = 1;
+        $prod2->price = 577000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Zinc';
+        $prod2->qty = 1;
+        $prod2->price = 135000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Diacont';
+        $prod2->qty = 1;
+        $prod2->price = 390000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Glucosamine';
+        $prod2->qty = 1;
+        $prod2->price = 218000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Shampoo';
+        $prod2->qty = 1;
+        $prod2->price = 54000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Conditioner';
+        $prod2->qty = 1;
+        $prod2->price = 60000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Spakare Shower Wash';
+        $prod2->qty = 1;
+        $prod2->price = 87000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Airiz Panty';
+        $prod2->qty = 1;
+        $prod2->price = 57000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Airiz Day';
+        $prod2->qty = 1;
+        $prod2->price = 43000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Airiz Night';
+        $prod2->qty = 1;
+        $prod2->price = 42000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Starter Kit + New ID';
+        $prod2->qty = 1;
+        $prod2->price = 99000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Odol';
+        $prod2->qty = 1;
+        $prod2->price = 90000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Pupuk';
+        $prod2->qty = 1;
+        $prod2->price = 103000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Tiens Health Mattress';
+        $prod2->qty = 1;
+        $prod2->price = 18200000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Tiens Oli Motor';
+        $prod2->qty = 1;
+        $prod2->price = 124000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Kemasan Perfect Mask';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Kemasan PBM';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Kemasan Perfect Eye + Bonus';
+        $prod2->qty = 1;
+        $prod2->price = 55000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Bonus Akupuntur';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Bonus Honey';
+        $prod2->qty = 1;
+        $prod2->price = 35000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Bonus Kacamata';
+        $prod2->qty = 1;
+        $prod2->price = 25000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Bonus Gel PM';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Bonus Gel PBM';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = 'Master Grow Up';
+        $prod2->qty = 1;
+        $prod2->price = 290000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+
+        $prod2 = new Product();
+        $prod2->name = '(MP) Master Weight Gain';
+        $prod2->qty = 1;
+        $prod2->price = 110000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = '(MP) V-Beauty';
+        $prod2->qty = 1;
+        $prod2->price = 125000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = '(MP) LQ-Man';
+        $prod2->qty = 1;
+        $prod2->price = 125000;
+        $prod2->type = 'Paket';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Master Grow Up';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse Master Weight';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse V-Beauty';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Dus Endorse LQ-Man';
+        $prod2->qty = 1;
+        $prod2->price = 30000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
+
+        $prod2 = new Product();
+        $prod2->name = 'Extra Packing Dus + Bubble Wrap';
+        $prod2->qty = 1;
+        $prod2->price = 10000;
+        $prod2->type = 'Pcs';
+        $prod2->save();
     }
 }
